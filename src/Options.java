12724a1e0c0d45067eae27a01802c2f1d5da406f
EwyBoy <ewyboy@gmail.com>
2016-09-29 03:46:06 +0200

    Progress - Ingress

---
+++ src/Options.java
@@ -0,0 +1,11 @@
+/**
+ * Created by EwyBoy
+ **/
+public class Options {
+
+    public static class WindowValues {
+        private static int WINDOW_SCALE = 2;
+        private static int WINDOW_HEIGHT = 540;
+        private static int WINDOW_WIDTH = 720;
+    }
+}
